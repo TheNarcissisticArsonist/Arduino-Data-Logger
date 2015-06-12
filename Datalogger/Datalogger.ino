a90a27db62aea9243105917de651dbde413b13eb
TheNarcissisticArsonist <TommyCohn116@gmail.com>
2015-06-12 10:41:00 -0400

    Starts SD card

--- Datalogger/Datalogger.ino
+++ Datalogger/Datalogger.ino
@@ -4,6 +4,16 @@
 void setup() {
   Serial.begin(9600);
   Serial.println("Serial connected");
+  if(SD.begin(4)) {
+    Serial.println("SD card connected through pin 4");
+  }
+  else {
+    Serial.println("");
+    Serial.println("");
+    Serial.println("");
+    Serial.println("ERROR: SD card could not connect via pin 4.");
+    while(true);
+  }
 }
 void loop() {
   
