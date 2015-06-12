e3559f4a9341c1418e839dabbe6b4719c0021938
TheNarcissisticArsonist <TommyCohn116@gmail.com>
2015-06-12 15:14:04 -0400

    Progress on zero air sample

--- Datalogger/Datalogger.ino
+++ Datalogger/Datalogger.ino
@@ -5,6 +5,11 @@ const int valve1 = 1; //Valve one is the valve closest to the sensor array, insi
 const int valve2 = 2; //Valve two is the valve that chooses between "Room Air" and "Zero Air"
 const int valve3 = 3; //Valve three is the valve that chooses between "Inter-System Air" and "Outlet Air"
 
+const int delayBetweenMeasurements = 120; //This is in seconds
+
+double zeroValueSensor1; //CO
+double zeroValueSensor2; //VOC
+
 void setup() {
   Serial.begin(9600);
   Serial.println("Serial connected");
@@ -26,7 +31,20 @@ void setup() {
   }
 }
 void loop() {
-  
+  String dataString = ""; //The data is formatted and dumped into this string, and then dumped into the log file
+  setValves("zero");
+  delay(1000 * delayBetweenMeasurements); //1000 milliseconds * x seconds
+  double sumValuesSensor1 = 0; //CO sensor
+  double sumValuesSensor2 = 0; //VOC sensor
+  for(int i=0; i<30; i++) {
+    sumValuesSensor1 += takeSample1(); //These get the total value
+    sumValuesSensor2 += takeSample2();
+    delay(1000);
+  }
+  double averageValue1 = sumValuesSensor1/30; //And this divides it by 30 (it takes one sample per second, for 30 seconds)
+  double averageValue2 = sumValuesSensor2/30;
+  zeroValueSensor1 = averageValue1; //And this sets the zero value to the average value we just found (this is zero air)
+  zeroValueSensor2 = averageValue2;
 }
 boolean setValves(String setting) {
   //setting is used to determine the configuration
@@ -55,4 +73,10 @@ boolean setValves(String setting) {
     digitalWrite(valve3, );*/
     Serial.println("Set valves for room air");
   }
+}
+double takeSample1() { //CO
+  
+}
+double takeSample2() { //VOC
+  
 }
