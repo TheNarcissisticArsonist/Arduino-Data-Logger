5fc41e15b44d2fa7f6e4062201a51dc14a0a80fd
TheNarcissisticArsonist <TommyCohn116@gmail.com>
2015-06-12 10:57:52 -0400

    Started writing setValves()

--- Datalogger/Datalogger.ino
+++ Datalogger/Datalogger.ino
@@ -1,9 +1,19 @@
 #include <SD.h>
 #include <SPI.h>
 
+const int valve1 = 1; //Valve one is the valve closest to the sensor array, inside of valves two and three
+const int valve2 = 2; //Valve two is the valve that chooses between "Room Air" and "Zero Air"
+const int valve3 = 3; //Valve three is the valve that chooses between "Inter-System Air" and "Outlet Air"
+
 void setup() {
   Serial.begin(9600);
   Serial.println("Serial connected");
+  
+  pinMode(valve1, OUTPUT);
+  pinMode(valve2, OUTPUT);
+  pinMode(valve3, OUTPUT);
+  Serial.println("Set pins controlling valves to output");
+  
   if(SD.begin(4)) {
     Serial.println("SD card connected through pin 4");
   }
@@ -17,4 +27,14 @@ void setup() {
 }
 void loop() {
   
+}
+boolean setValves(String setting) {
+  //setting is used to determine the configuration
+  //Valid options are shown in the switch statement below
+  if(setting == "zero air") {
+    digitalWrite(valve1, LOW);
+    digitalWrite(valve2, HIGH);
+    //This turns off valve 1 and turns valve 2 on, making a path from zero air to the VOC sensor
+    Serial.println("Set valves for zero air");
+  }
 }
