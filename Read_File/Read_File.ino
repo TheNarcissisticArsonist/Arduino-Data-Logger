b13c005538e617c24ea371daaabe2aaff588c9eb
TheNarcissisticArsonist <TommyCohn116@gmail.com>
2015-06-15 22:23:11 -0400

    Is this the real life? Is this just fantasy?

--- Read_File/Read_File.ino
+++ Read_File/Read_File.ino
@@ -6,6 +6,9 @@ void setup() {
   Serial.begin(9600);
   SD.begin(4);
   myFile = SD.open("");
+  Serial.println("");
+  Serial.println("");
+  Serial.println("");
   while(myFile.available()) {
     Serial.write(myFile.read());
   }
