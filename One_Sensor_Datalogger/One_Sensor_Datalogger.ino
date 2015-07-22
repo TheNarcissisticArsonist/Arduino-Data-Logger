025a664274d7c38b1354d80afe9379d6ab5b13cf
TheNarcissisticArsonist <TommyCohn116@gmail.com>
2015-07-21 22:44:31 -0400

    Connect to WiFi.

--- One_Sensor_Datalogger/One_Sensor_Datalogger.ino
+++ One_Sensor_Datalogger/One_Sensor_Datalogger.ino
@@ -15,12 +15,34 @@
 #include <WiFi.h>     //Used for WiFi connectivity
 #include <WiFiUdp.h>  //Used for UDP -- the protocal to get date and time
 
+/*
+-----THESE VALUES MUST BE CHANGED FOR EACH ARDUINO-----
+*/
 const int systemId = 12345; //This is the system ID. It's used a lot on the server
 const String key = "INSERT KEY HERE"; //This is the key used to access the database. Shhhh! It's a secret!
+
+const char ssid[] = "RedSox2";    //This is my WiFi network name and password.
+const char pass[] = "fenway1999"; //These will need to be changed by location.
+//const char key[] = "";
+//const int keyIndex = 0;
+/* --WiFi Instructions
+ * Open network: Only SSID is needed. Set const int wifiType to 0
+ * WPA2 personal network: SSID and password are needed. Set const int wifiType to 1
+ * WEP networks: SSID, key, and key index are needed. Set const int wifiType to 2
+ * All other networks are incompatible with Arduino.
+ */
+ const int wifiType = 1;
+
+/*
+-----END UNIQUE VALUES-----
+*/
 
 const int valve1 = 1; //The pin for the valve that switches between _____ (on) and _____ (off)
 const int valve2 = 2; //The pin for the valve that switches between _____ (on) and _____ (off)
 const int valve3 = 3; //The pin for the valve that switches between _____ (on) and _____ (off)
+
+int status = WL_IDLE_STATUS; //This variable is used to get the status of the Arduino's WiFi connection
+
 
 void setup() { //Run once at the beginning
   Serial.begin(9600); //The serial statement is used primarily for debugging.
@@ -42,6 +64,9 @@ void setup() { //Run once at the beginning
    * than the arduino can supply.
    */
 
+   /*
+   -----SD CARD-----
+   */
    if(SD.begin(4)) { //This starts up Arduino communication with the SD card on pin 4.
                      //Pin 4 is standard for the Arduino Uno Rev 3
                      //If successful, it returns true, allowing this conditional to work
@@ -60,6 +85,35 @@ void setup() { //Run once at the beginning
       * This is the closest way you can come to stopping an Arduino
       * without literally unplugging it.
       */
+   }
+
+   /*
+   -----WIFI-----
+   */
+   Serial.println("Attempting to connect to WiFi");
+   Serial.print("Network name: ");
+   Serial.println(ssid);
+   Serial.print("Password: ");
+   Serial.println(pass);
+   Serial.print("Network type: ");
+   Serial.println(wifiType);
+   while(status != WL_CONNECTED) { //WL_CONNECTED is the status given when the WiFi is connected and all is good.
+                                   //The green LED on the WiFi shield lights up when it's connected.
+     Serial.print("Not yet connected. Status: ");
+     Serial.println(status);
+
+     switch(wifiType) {
+       case 0: //Open network
+        status = WiFi.begin(ssid);
+        break;
+      case 1: //WPA2 personal network
+        status = WiFi.begin(ssid, pass);
+        break;
+      case 2: //WEP network
+        status = WiFi.begin(ssid, keyIndex, key);
+        break;
+     }
+     delay(3000); //Wait 3 seconds before trying again
    }
 }
 void loop() { //Loop for all eternity
