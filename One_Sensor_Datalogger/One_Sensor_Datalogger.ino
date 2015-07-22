6b1bf2d677a5eef366e217627123d20470eb147b
TheNarcissisticArsonist <TommyCohn116@gmail.com>
2015-07-21 22:06:03 -0400

    Added some constants.

--- One_Sensor_Datalogger/One_Sensor_Datalogger.ino
+++ One_Sensor_Datalogger/One_Sensor_Datalogger.ino
@@ -3,3 +3,10 @@
 #include <SPI.h>      //Used by WiFi and the SD card -- they both use SPI to communicate
 #include <WiFi.h>     //Used for WiFi connectivity
 #include <WiFiUdp.h>  //Used for UDP -- the protocal to get date and time
+
+const int systemId = 12345; //This is the system ID. It's used a lot on the server
+const String key = "INSERT KEY HERE"; //This is the key used to access the database. Shhhh! It's a secret!
+
+const int valve1 = 1; //The valve that switches between _____ (on) and _____ (off)
+const int valve2 = 2; //The valve that switches between _____ (on) and _____ (off)
+const int valve3 = 3; //The valve that switches between _____ (on) and _____ (off)
