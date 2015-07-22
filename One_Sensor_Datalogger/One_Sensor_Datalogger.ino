34e7152fa423574d40eb6bb3db3390da319965d6
TheNarcissisticArsonist <TommyCohn116@gmail.com>
2015-07-21 21:57:29 -0400

    Includes statements.'

---
+++ One_Sensor_Datalogger/One_Sensor_Datalogger.ino
@@ -0,0 +1,5 @@
+//Include statements
+#include <SD.h>       //Used for the SD card
+#include <SPI.h>      //Used by WiFi and the SD card -- they both use SPI to communicate
+#include <WiFi.h>     //Used for WiFi connectivity
+#include <WiFiUdp.h>  //Used for UDP -- the protocal to get date and time
