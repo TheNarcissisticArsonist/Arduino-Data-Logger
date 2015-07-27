eb91b40d58b58fbba456ad349fe082d8c8911407
TheNarcissisticArsonist <TommyCohn116@gmail.com>
2015-07-27 14:48:44 -0400

    Wifi stuff.

--- Final_Datalogger/Final_Datalogger.ino
+++ Final_Datalogger/Final_Datalogger.ino
@@ -20,5 +20,17 @@
 ----- Values unique to each sensor package...
 ----- These will need to be changed for each one.
 */
-const int systemId = 12345; //The ID tag of the system
-const String key = "super secret password" //The password to get into the server
+const int systemId = 12345;                 //The ID tag of the system
+const String key = "super secret password"  //The password to get into the server
+
+char ssid[] = "RedSox2";      //Network name
+char pass[] = "fenway1999";   //Password
+char key = "";                //Used for WEP
+int keyIndex = 0;             //Used for WEP
+const int wifiType = 1;
+/*
+ * Open Network: only ssid is needed. Set wifiType to 0.
+ * WPA or WPA2 Personal: ssid and pass are needed. Set wifiType to 1.
+ * WEP: ssid, key, and keyIndex are needed. Set wifiType to 2.
+ * Other networks are incompatible.
+ */
