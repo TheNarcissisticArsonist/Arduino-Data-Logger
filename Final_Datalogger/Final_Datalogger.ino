bd7a051c4ec2ea29880075899783078648fcef0c
TheNarcissisticArsonist <TommyCohn116@gmail.com>
2015-07-27 14:40:43 -0400

    Include.

---
+++ Final_Datalogger/Final_Datalogger.ino
@@ -0,0 +1,17 @@
+/*
+ * Code written by Thomas Cohn.
+ * Insert more personalized comments and such here...
+ */
+
+/*
+ * Do not worry!
+ * Comments are removed during compiling, and
+ * therefore have no effect on memory usage.
+ */
+
+ //Include statements
+ #include <SD.h>       //Used for the SD card
+ #include <SPI.h>      //Used by WiFi and the SD card -- they both use SPI to communicate
+ #include <WiFi.h>     //Used for WiFi connectivity
+ /* WARNING: WRONG WIFI LIBRARY! */
+ /* Use this one instead: https://learn.adafruit.com/adafruit-cc3000-wifi/cc3000-library-software */
