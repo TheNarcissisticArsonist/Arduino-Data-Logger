7a5a7b956162a65cd96228e744e67a1ec184f8f3
TheNarcissisticArsonist <TommyCohn116@gmail.com>
2015-07-27 14:45:40 -0400

    Added some unique values.

--- Final_Datalogger/Final_Datalogger.ino
+++ Final_Datalogger/Final_Datalogger.ino
@@ -15,3 +15,10 @@
 #include <WiFi.h>     //Used for WiFi connectivity
 /* WARNING: WRONG WIFI LIBRARY! */
 /* Use this one instead: https://learn.adafruit.com/adafruit-cc3000-wifi/cc3000-library-software */
+
+/*
+----- Values unique to each sensor package...
+----- These will need to be changed for each one.
+*/
+const int systemId = 12345; //The ID tag of the system
+const String key = "super secret password" //The password to get into the server
