3ea2fe4049859951bf15002057c29e68332b92ba
TheNarcissisticArsonist <TommyCohn116@gmail.com>
2015-07-27 14:44:23 -0400

    That was weird...

--- Final_Datalogger/Final_Datalogger.ino
+++ Final_Datalogger/Final_Datalogger.ino
@@ -9,9 +9,9 @@
  * therefore have no effect on memory usage.
  */
 
- //Include statements
- #include <SD.h>       //Used for the SD card
- #include <SPI.h>      //Used by WiFi and the SD card -- they both use SPI to communicate
- #include <WiFi.h>     //Used for WiFi connectivity
- /* WARNING: WRONG WIFI LIBRARY! */
- /* Use this one instead: https://learn.adafruit.com/adafruit-cc3000-wifi/cc3000-library-software */
+//Include statements
+#include <SD.h>       //Used for the SD card
+#include <SPI.h>      //Used by WiFi and the SD card -- they both use SPI to communicate
+#include <WiFi.h>     //Used for WiFi connectivity
+/* WARNING: WRONG WIFI LIBRARY! */
+/* Use this one instead: https://learn.adafruit.com/adafruit-cc3000-wifi/cc3000-library-software */
