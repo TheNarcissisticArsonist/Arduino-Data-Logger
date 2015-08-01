309b7c449faaf33477d5daae6980c3f24216cc34
TheNarcissisticArsonist <TommyCohn116@gmail.com>
2015-07-31 22:21:34 -0400

    Include statements.

---
+++ Demonstration/Demonstration.ino
@@ -0,0 +1,15 @@
+/*
+ * Code written by Thomas Cohn.
+ * Insert more personalized comments and such here...
+ */
+
+/*
+ * Do not worry!
+ * Comments are removed during compiling, and
+ * therefore have no effect on memory usage.
+ */
+
+//Include statements
+#include <SD.h>       //Used for the SD card
+#include <SPI.h>      //Used by WiFi and the SD card -- they both use SPI to communicate
+#include <WiFi.h>     //Used for WiFi connectivity
