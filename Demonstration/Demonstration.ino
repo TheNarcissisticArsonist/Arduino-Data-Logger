1efde42cc38fe366b29c7dd70db96b92235a2f77
TheNarcissisticArsonist <TommyCohn116@gmail.com>
2015-07-31 22:25:19 -0400

    Void setup.

--- Demonstration/Demonstration.ino
+++ Demonstration/Demonstration.ino
@@ -121,3 +121,83 @@ double takeMeasurement() {
   }
   return ((double)sum)/measureTime;
 }
+
+void setup() { //Run once at the beginning
+  Serial.begin(9600); //The serial statement is used primarily for debugging.
+                      //Throughout the code, you may see statements such as Serial.print and Serial.println
+                      //These are simply messages being printed out to the computer connected to the Arduino
+
+  Serial.println(); //The serial monitor often contains some garbled messages
+  Serial.println(); //upon starting up. This simply prints a few lines so the
+  Serial.println(); //programmer knows when the actual information is coming.
+
+  pinMode(valve1, OUTPUT); //This sets the pins controlling
+  pinMode(valve2, OUTPUT); //the various valves defined as
+  pinMode(valve3, OUTPUT); //constants above.
+  /*
+   * Incidentally, these aren't actually directly
+   * controlling the valves, but rather controlling
+   * relays that control the valves. This is because
+   * the valves require a higher voltage and current
+   * than the arduino can supply.
+   */
+
+   /*
+   -----SD CARD-----
+   */
+   if(SD.begin(4)) { //This starts up Arduino communication with the SD card on pin 4.
+                     //Pin 4 is standard for the Arduino Uno Rev 3
+                     //If successful, it returns true, allowing this conditional to work
+     Serial.println("SD card connected through pin 4.");
+   }
+   else {
+     Serial.println("ERROR: Could not connect to SD card.");
+     while(true); //This effectively stops the Arduino
+     /*
+      * This is an unusual piece of code to find in most languages.
+      * However, an Arduino can never truly "stop" -- it must always.
+      * be doing something. Stopping a program is important if there's
+      * a problem, as this prevents anything further from breaking and
+      * gives the programmer a chance to fix the problem.
+      *
+      * This is the closest way you can come to stopping an Arduino
+      * without literally unplugging it.
+      */
+   }
+
+   /*
+   -----WIFI-----
+   */
+   Serial.println("Attempting to connect to WiFi");
+   Serial.print("Network name: ");
+   Serial.println(ssid);
+   Serial.print("Password: ");
+   Serial.println(pass);
+   Serial.print("Network type: ");
+   Serial.println(wifiType);
+   while(status != WL_CONNECTED) { //WL_CONNECTED is the status given when the WiFi is connected and all is good.
+                                   //The green LED on the WiFi shield lights up when it's connected.
+     Serial.print("Not yet connected. Status: ");
+     Serial.println(status);
+
+     switch(wifiType) {
+       case 0: //Open network
+        status = WiFi.begin(ssid);
+        break;
+      case 1: //WPA2 personal network
+        status = WiFi.begin(ssid, pass);
+        break;
+      /*case 2: //WEP network
+        status = WiFi.begin(ssid, keyIndex, key);
+        break;*/
+      //Keep that last one commented unless you're using WEP.
+      //Compile errors... BEWARE
+     }
+     delay(3000); //Wait 3 seconds before trying again
+   }
+   Serial.println("Wifi is connected.");
+   
+   Serial.println("Completed the setup function.");
+   Serial.println();
+   Serial.println();
+}
